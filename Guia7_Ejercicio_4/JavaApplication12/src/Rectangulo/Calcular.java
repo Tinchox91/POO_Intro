edd032e0d365c1e65a26ee7d39dc98946a7293f9
Tincho <tinchox1991@gmail.com>
2023-04-01 16:45:09 -0300

    ejercicio4

---
+++ Guia7_Ejercicio_4/JavaApplication12/src/Rectangulo/Calcular.java
@@ -0,0 +1,15 @@
+
+package Rectangulo;
+
+
+public class Calcular {
+
+   
+    public static void main(String[] args) {
+       Rectangulo r = new Rectangulo();
+       r.ingresarDatos();
+       r.dibujarRectangulo();
+       
+    }
+    
+}
